7734e435c1c2a2cd412fc568d71840f0bbc0aa95
Aylin Abdollahi <97289354+aylinabdl@users.noreply.github.com>
2023-08-17 10:35:10 +0330

    Initial commit

---
+++ src/Stats.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const precentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {precentage === 100
+          ? "you got everything! ready to go"
+          : `You have ${numItems} items on your list , 
+        and you already packed ${numPacked} (${precentage}%) .`}
+      </em>
+    </footer>
+  );
+}
